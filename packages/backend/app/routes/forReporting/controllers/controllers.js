779ffa661f7c592c0eb254d0d00130fbba7939fa
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-04-21 18:48:43 +0600

    #27 рефактор кода

--- packages/backend/app/routes/forReporting/controllers/controllers.js
+++ packages/backend/app/routes/forReporting/controllers/controllers.js
@@ -84,7 +84,7 @@ const ReportingControllers = {
             async function somefun() {
               const response = await sequelize.query(
                 `SELECT priority, count(*) as COUNTER
-              FROM database_esdp.request
+              FROM database-esdp.request
               GROUP BY priority;`,
                 { raw: true, type: sequelize.QueryTypes.SELECT }
               );

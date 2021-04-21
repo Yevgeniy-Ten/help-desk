a4983ebc31fa5c8219407c9d701d6c9a118faf5e
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-04-21 18:44:19 +0600

    #27 рефактор кода

--- packages/backend/app/routes/forReporting/reporting.router.js
+++ packages/backend/app/routes/forReporting/reporting.router.js
@@ -1,10 +1,8 @@
 const { Router } = require("express");
-const auth = require("../../middlewares/auth");
 const ReportingControllers = require("./controllers/controllers");
 
 const reportingRouter = Router();
 
 reportingRouter.get("/history", ReportingControllers.getRequestHistory);
-
 
 module.exports = reportingRouter;

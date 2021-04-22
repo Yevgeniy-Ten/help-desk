9519053b21cb9ca952b0a5cd5807e0025a6ff737
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-04-22 19:28:13 +0600

    #32 добавил кнопку назад при редактировании заявки

--- packages/frontend/src/components/AppealDetails/AppealDetails.js
+++ packages/frontend/src/components/AppealDetails/AppealDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Space } from "antd";
+import { Button, Card } from "antd";
 import { NavLink } from "react-router-dom";
 import "./AppealDetails.css";
 
--- packages/frontend/src/containers/Appeals/EditAppealPage/EditAppealPage.jsx
+++ packages/frontend/src/containers/Appeals/EditAppealPage/EditAppealPage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
+import { Button, Row, Col } from "antd";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { getTopics, getDepartments } from "../../Settings/redux/settingGetters";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import EditAppealForm from "../../../components/CreateForms/EditAppealForm";
 import { fetchAppeal, fetchPutAppeal } from "../redux/appealActions";
 import { getAppealCurrent, getAppealStateLoader } from "../redux/appealGetters";
@@ -43,14 +44,23 @@ const EditAppealPage = () => {
       ) : (
         <>
           {appeal ? (
-            <EditAppealForm
-              appealFields={appeal}
-              topics={topics}
-              departments={departments}
-              employees={users}
-              onChangeFields={onChangeFields}
-              onSaveAppeal={onSaveAppeal}
-            />
+            <Row style={{ padding: "20px 0 0 20px" }}>
+              <Col span={12} className="mb-sm">
+                <Button key="3" type="primary">
+                  <NavLink to="/appeals">Назад</NavLink>
+                </Button>
+              </Col>
+              <Col span={24} className="mb-sm">
+                <EditAppealForm
+                  appealFields={appeal}
+                  topics={topics}
+                  departments={departments}
+                  employees={users}
+                  onChangeFields={onChangeFields}
+                  onSaveAppeal={onSaveAppeal}
+                />
+              </Col>
+            </Row>
           ) : (
             <p>Заявка не найдена</p>
           )}

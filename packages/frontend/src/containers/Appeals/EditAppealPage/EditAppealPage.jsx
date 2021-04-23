8da7bbdb613f80d5fb7842ca69747147f68d3e9f
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-04-23 11:53:45 +0600

    #32 Перенес страницу редактирования заявки на Drawer на фронте

--- packages/frontend/src/containers/Appeals/EditAppealPage/EditAppealPage.jsx
+++ packages/frontend/src/containers/Appeals/EditAppealPage/EditAppealPage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
-import { Button, Row, Col } from "antd";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { getTopics, getDepartments } from "../../Settings/redux/settingGetters";
-import { useParams, NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import EditAppealForm from "../../../components/CreateForms/EditAppealForm";
 import { fetchAppeal, fetchPutAppeal } from "../redux/appealActions";
 import { getAppealCurrent, getAppealStateLoader } from "../redux/appealGetters";
@@ -44,23 +43,14 @@ const EditAppealPage = () => {
       ) : (
         <>
           {appeal ? (
-            <Row style={{ padding: "20px 0 0 20px" }}>
-              <Col span={12} className="mb-sm">
-                <Button key="3" type="primary">
-                  <NavLink to="/appeals">Назад</NavLink>
-                </Button>
-              </Col>
-              <Col span={24} className="mb-sm">
-                <EditAppealForm
-                  appealFields={appeal}
-                  topics={topics}
-                  departments={departments}
-                  employees={users}
-                  onChangeFields={onChangeFields}
-                  onSaveAppeal={onSaveAppeal}
-                />
-              </Col>
-            </Row>
+            <EditAppealForm
+              appealFields={appeal}
+              topics={topics}
+              departments={departments}
+              employees={users}
+              onChangeFields={onChangeFields}
+              onSaveAppeal={onSaveAppeal}
+            />
           ) : (
             <p>Заявка не найдена</p>
           )}

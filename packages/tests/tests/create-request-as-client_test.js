00f5285979209b8e4612dd8995348dfd730ab0f9
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-05-09 15:22:45 +0600

    #50 refact code, create-request feature

--- packages/tests/tests/create-request-as-client_test.js
+++ packages/tests/tests/create-request-as-client_test.js
@@ -1,63 +1,129 @@
-Feature('create-request-as-client');
+Feature("create-request-as-client");
 
-Scenario('Создание заявки от клиента', ({ I }) => {
-    When(/^я нахожусь на странице "(.*?)"$/, () => {
-        // From "features/create-request.feature" {"line":14,"column":5}
-        throw new Error("Not implemented yet");
-    });
+Scenario("Создание заявки от клиента", ({ I }) => {
+  //  вход на сайт HelpDesk
+  Given(/^я нахожусь на странице входа "(.*?)"$/, () => {
+    // From "features/create-request.feature" {"line":14,"column":5}
+    I.amOnPage("/auth");
+    I.waitForElement("form", 10);
+  });
 
-    Then("почта пользователя {string} будет введено в поле с айди {string}", () => {
-        // From "features/create-request.feature" {"line":8,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Then("пароль пользователя {string} будет введено в поле с айди {string}", () => {
-        // From "features/user-login.feature" {"line":8,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Then("я нажимаю на кнопку с текстом {string}", () => {
-        // From "features/create-request.feature" {"line":15,"column":5}
-        throw new Error("Not implemented yet");
-    });
+  When(
+    "почта пользователя {string} будет введено в поле с айди {string}",
+    (value, fieldName) => {
+      // From "features/create-request.feature" {"line":8,"column":5}
+      I.fillField({ id: fieldName }, value);
+    }
+  );
+  When(
+    "пароль пользователя {string} будет введено в поле с айди {string}",
+    (value, fieldName) => {
+      // From "features/user-login.feature" {"line":8,"column":5}
+      I.fillField({ id: fieldName }, value);
+    }
+  );
+  When("я нажимаю на кнопку с текстом {string}", (button) => {
+    // From "features/create-request.feature" {"line":15,"column":5}
+    I.click({ id: button });
+  });
 
-    Then("я жду пока пользователь зайдет в сессию", () => {
-        // From "features/create-request.feature" {"line":11,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Given(/^я нахожусь на странице "(.*?)"$/, () => {
-        // From "features/user-register.feature" {"line":5,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Then("я нажимаю на кнопку с текстом {string}", () => {
-        // From "features/user-register.feature" {"line":12,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Given("выберу тематику заявки {string} из списка тематик", () => {
-        // From "features/create-request.feature" {"line":16,"column":5}
-        throw new Error("Not implemented yet");
-    });
+  When("я жду пока пользователь зайдет в сессию", () => {
+    // From "features/create-request.feature" {"line":11,"column":5}
+    I.wait(8);
+  });
+  // ------------------------------------------------------------
+  //   новая заявка
+  When(/^я нахожусь на странице "(.*?)"$/, () => {
+    // From "features/user-register.feature" {"line":5,"column":5}
+    I.amOnPage("/appeals");
+  });
+  When("я нажимаю на кнопку с текстом {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+  });
+  When("я нажимаю на селект тематики {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+  });
+  When(
+    "выберу тематику заявки {string} из списка тематик {string}",
+    (text, elementDiv) => {
+      // From "features/create-request.feature" {"line":16,"column":5}
+      I.wait(1);
+      const element = locate(elementDiv).withAttr({ name: text });
+      I.click(element);
+    }
+  );
 
-    Then("введу в поле с айди {string} текст {string}", () => {
-        // From "features/create-request.feature" {"line":17,"column":5}
-        throw new Error("Not implemented yet");
-    });
+  When("я нажимаю на селект клиента {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+  });
+  When(
+    "выберу клиента заявки {string} из списка клиентов {string}",
+    (text, elementDiv) => {
+      // From "features/create-request.feature" {"line":16,"column":5}
+      I.wait(1);
+      const element = locate(elementDiv).withAttr({ name: text });
+      I.click(element);
+    }
+  );
 
-    Then("выберу приоритет тематики {string} из списка приоритетов", () => {
-        // From "features/create-request.feature" {"line":18,"column":5}
-        throw new Error("Not implemented yet");
-    });
+  When("я нажимаю на селект статуса {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+  });
+  When(
+    "выберу статус заявки {string} из списка статусов {string}",
+    (text, elementDiv) => {
+      // From "features/create-request.feature" {"line":16,"column":5}
+      I.wait(1);
+      const element = locate(elementDiv).withAttr({ name: text });
+      I.click(element);
+    }
+  );
 
-    Then("введу в поле с айди {string} текст {string}", () => {
-        // From "features/create-request.feature" {"line":19,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    When("я нажимаю на кнопку с текстом {string}", () => {
-        // From "features/user-register.feature" {"line":12,"column":5}
-        throw new Error("Not implemented yet");
-    });
-    Then(/^вижу эту свою заявку с заголовком "(.*?)" в списке заявок на странице "(.*?)"$/, () => {
-        // From "features/create-request.feature" {"line":21,"column":5}
-        throw new Error("Not implemented yet");
-    });
+  When(
+    "введу в поле с заголовка {string} текст {string}",
+    (fieldName, value) => {
+      // From "features/create-request.feature" {"line":17,"column":5}
+      I.fillField({ id: fieldName }, value);
+    }
+  );
 
+  When("я нажимаю на селект приоритета {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+  });
+  When(
+    "выберу приоритет заявки {string} из списка приоритетов {string}",
+    (text, elementDiv) => {
+      // From "features/create-request.feature" {"line":16,"column":5}
+      I.wait(1);
+      const element = locate(elementDiv).withAttr({ name: text });
+      I.click(element);
+    }
+  );
 
+  When("введу в поле с айди {string} текст {string}", (fieldName, value) => {
+    // From "features/create-request.feature" {"line":19,"column":5}
+    I.fillField({ id: fieldName }, value);
+  });
+
+  When("я нажимаю на кнопку с текстом {string}", (button) => {
+    // From "features/user-register.feature" {"line":12,"column":5}
+    I.click({ id: button });
+    I.wait(7);
+  });
+
+  When(/^меня переместит на страницу"(.*?)"$/, () => {
+    // From "features/register.feature" {"line":5,"column":1}
+    I.amOnPage("/appeals");
+  });
+
+  Then("увижу созданную заявку от {string}", (text) => {
+    // From "features/register.feature" {"line":12,"column":1}
+    I.see(text);
+    I.wait(1);
+  });
 });

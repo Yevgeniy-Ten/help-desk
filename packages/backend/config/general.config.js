dec040e27560cadd5d132e6c6ab330fc1fe9d6b0
Yevgeniy T <foodila@hotmail.com>
2021-05-09 21:47:49 +0600

    #49 Init sendmail functionallity and bundle with user register

--- packages/backend/config/general.config.js
+++ packages/backend/config/general.config.js
@@ -3,14 +3,15 @@ require("dotenv").config();
 // webURL: `http://localhost:${process.env.FRONT_PORT || 3000}`,
 
 // webURL: `http://${process.env.FRONT_HOST || "localhost"}:${process.env.FRONT_PORT || 3000}`, при таком назначении видимо надо подключать nginx поэтому общение идет через мой компьютер
-
+const HOST = `http://${process.env.FRONT_HOST || "localhost"}`
 module.exports = {
-  facebook: {
-    appID: "",
-    appSecret: "",
-  },
-  constants: [],
-  webURL: `http://${process.env.FRONT_HOST || "localhost"}:${
-    process.env.FRONT_PORT || 3000
-  }`,
+    facebook: {
+        appID: "",
+        appSecret: "",
+    },
+    constants: [],
+    backUrl: `${HOST}:${process.env.BACK_PORT || 3003}`,
+    webURL: `${HOST}:${
+        process.env.FRONT_PORT || 3000
+    }`,
 };

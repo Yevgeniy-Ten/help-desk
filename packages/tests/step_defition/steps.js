58c10b9888b9337cd970c16c97917b4dec6b45ec
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-05-11 18:25:24 +0600

    #50 исправил ошибки

--- packages/tests/step_defition/steps.js
+++ packages/tests/step_defition/steps.js
@@ -4,7 +4,7 @@ const { I } = inject();
 Given("I have a defined step", () => {
   // TODO: replace with your own step
 });
-
+//  login
 Given(/^я нахожусь на странице входа "(.*?)"$/, () => {
   I.amOnPage("/auth");
   I.waitForElement("form", 10);
@@ -12,13 +12,13 @@ Given(/^я нахожусь на странице входа "(.*?)"$/, () => {
 
 When(
   "почта пользователя {string} будет введено в поле с айди {string}",
-  (value, fieldName) => {
+  (fieldName, value) => {
     I.fillField({ id: fieldName }, value);
   }
 );
 When(
   "пароль пользователя {string} будет введено в поле с айди {string}",
-  (value, fieldName) => {
+  (fieldName, value) => {
     I.fillField({ id: fieldName }, value);
   }
 );
@@ -30,91 +30,95 @@ When("я жду пока пользователь зайдет в сессию",
   I.wait(3);
 });
 // ------------------------------------------------------------
-//   новая заявка
-When("я нахожусь на странице {string}", (url) => {
+//   новый user
+When("я нахожусь на странице заявок {string}", (url) => {
   I.seeCurrentUrlEquals(url);
 });
-When("я нажимаю кнопку с текстом {string}", (button) => {
+// жму контакты
+When(
+  "я нажимаю на ссылку {string} аттрибут {string} с текстом {string}",
+  (a, attr, text) => {
+    I.wait(1);
+    const element = locate(a).withAttr({ href: attr }).withText(text);
+    I.click(element);
+    I.wait(1);
+  }
+);
+
+When("я перемещусь на страницу контакты {string}", (text) => {
+  I.wait(2);
+  I.see(text);
+  I.wait(1);
+});
+//  кнопка "Создать пользователя"
+When("я нажму на кнопку с текстом {string}", (button) => {
   I.click(button);
 });
-When("я нажимаю на селект тематики {string}", (button) => {
-  I.click({ id: button });
+// проверка url
+When("я перемещусь на страницу форма создания {string}", (url) => {
+  I.seeCurrentUrlEquals(url);
 });
-When(
-  "выберу тематику заявки {string} из списка тематик {string}",
-  (text, elementDiv) => {
-    I.wait(1);
-    const element = locate(elementDiv).withAttr({ title: text });
-    I.click(element);
-  }
-);
-
-When("я нажимаю на селект клиента {string}", (button) => {
-  // From 'features/user-register.feature' {'line':12,'column':5}
-  I.click({ id: button });
-});
-When(
-  "выберу клиента заявки {string} из списка клиентов {string} с классом {string}",
-  (text, elementDiv) => {
-    I.wait(1);
-    const element = locate(elementDiv).withAttr({ name: text });
-    I.click(element);
-    I.wait(1);
-  }
-);
-
-When("я нажимаю на селект статуса {string}", (button) => {
-  // From 'features/user-register.feature' {'line':12,'column':5}
-  I.click({ id: button });
-});
-When(
-  "выберу статус заявки {string} из списка статусов {string}",
-  (text, elementDiv) => {
-    // From 'features/create-request.feature' {'line':16,'column':5}
-    I.wait(1);
-    const element = locate(elementDiv).withAttr({ title: text });
-    I.click(element);
-    I.wait(1);
-  }
-);
-
-When("введу в поле заголовка {string} текст {string}", (fieldName, value) => {
-  // From 'features/create-request.feature' {'line':17,'column':5}
+// имя
+When("введу в поле с айди {string} текст {string}", (fieldName, value) => {
   I.fillField({ id: fieldName }, value);
   I.wait(1);
 });
-
-When("я нажимаю на селект приоритета {string}", (button) => {
-  // From 'features/user-register.feature' {'line':12,'column':5}
+//  фам
+When(
+  "введу в поле с айдишником {string} текст {string}",
+  (fieldName, value) => {
+    I.fillField({ id: fieldName }, value);
+    I.wait(1);
+  }
+);
+//  мэйл
+When(
+  "введу в поле email с айди {string} текст {string}",
+  (fieldName, value) => {
+    I.fillField({ id: fieldName }, value);
+    I.wait(1);
+  }
+);
+// тел. номер
+When(
+  "введу в поле телефон с айди {string} номер {string}",
+  (fieldName, value) => {
+    I.fillField({ id: fieldName }, value);
+    I.wait(1);
+  }
+);
+// выбор компании
+When("я нажимаю на селект комапнии {string}", (button) => {
   I.click({ id: button });
 });
+
 When(
-  "выберу приоритет заявки {string} из списка приоритетов {string}",
+  "выберу комапнию {string} из списка комапнии {string}",
   (text, elementDiv) => {
-    // From 'features/create-request.feature' {'line':16,'column':5}
     I.wait(1);
     const element = locate(elementDiv).withAttr({ title: text });
     I.click(element);
     I.wait(1);
   }
 );
-
-When("введу в поле с айди {string} текст {string}", (fieldName, value) => {
-  // From 'features/create-request.feature' {'line':19,'column':5}
-  I.fillField({ id: fieldName }, value);
-});
-
-When("я нажимаю на кнопку с текстом {string}", (button) => {
-  // From 'features/user-register.feature' {'line':12,'column':5}
+// пароль
+When(
+  "введу в поле пароля с айди {string} пароль {string}",
+  (fieldName, value) => {
+    I.fillField({ id: fieldName }, value);
+    I.wait(1);
+  }
+);
+//  кнопка создать
+When("я нажиму на кнопку с текстом {string}", (button) => {
   I.click(button);
-  I.wait(4);
 });
-
+// /users
 When("меня переместит на страницу {string}", (url) => {
   I.seeCurrentUrlEquals(url);
 });
 
-Then("увижу созданную заявку от {string}", (text) => {
+Then("увижу сообщение {string}", (text) => {
   // From 'features/register.feature' {'line':12,'column':1}
   I.see(text);
   I.wait(1);

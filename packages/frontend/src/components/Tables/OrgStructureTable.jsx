407e4b5eb3297705b2f7e5108d65c7259640cc6f
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-05-16 10:21:41 +0600

    #56 в таблице орг.структура добавил фильтр(столб. отдел, главный)

--- packages/frontend/src/components/Tables/OrgStructureTable.jsx
+++ packages/frontend/src/components/Tables/OrgStructureTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Spinner from "../Spinner/Spinner";
 import { Button, Form, Popconfirm, Space, Table, Typography } from "antd";
 import EditableCell from "../UI/EditableCeil";
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   getEditableElement,
   getOrgStructures,
+  getDepartments,
   getSettingsLoader
 } from "../../containers/Settings/redux/settingGetters";
 import { fetchSettings } from "../../containers/Settings/redux/settingsActions";
@@ -13,11 +14,23 @@ import { fetchSettings } from "../../containers/Settings/redux/settingsActions";
 const OrgStructureTable = ({ onShowEditor }) => {
   const dispatch = useDispatch();
   const orgStructures = useSelector(getOrgStructures);
+  const departments = useSelector(getDepartments);
   const isLoad = useSelector(getSettingsLoader);
-
+  const [filtersDepartment, setFiltersDepartment] = useState([]);
   useEffect(() => {
+    dispatch(fetchSettings("departments"));
     dispatch(fetchSettings("orgstructure"));
   }, [dispatch]);
+
+  useMemo(() => {
+    if (departments) {
+      const departmentsCopy = departments.map((department) => {
+        return { text: department.title, value: department.title };
+      });
+      setFiltersDepartment([...departmentsCopy]);
+      console.log(filtersDepartment);
+    }
+  }, [departments]);
   const columns = [
     {
       title: "Идентификатор",
@@ -36,6 +49,13 @@ const OrgStructureTable = ({ onShowEditor }) => {
       title: "Отдел",
       dataIndex: "department",
       key: "department",
+      filters: filtersDepartment,
+      // eslint-disable-next-line consistent-return
+      onFilter: (value, record) => {
+        if (record.department) {
+          return record.department.title.indexOf(value) === 0;
+        }
+      },
       render: (department) => {
         return department ? department.title : "null";
       }
@@ -44,6 +64,22 @@ const OrgStructureTable = ({ onShowEditor }) => {
       title: "Главный",
       dataIndex: "isMain",
       key: "isMain",
+      filters: [
+        {
+          text: "Да",
+          value: true
+        },
+        {
+          text: "Нет",
+          value: false
+        }
+      ],
+      // eslint-disable-next-line consistent-return
+      onFilter: (value, record) => {
+        if (record.isMain === value) {
+          return true;
+        }
+      },
       render: (isMain) => {
         return isMain ? "Да" : "Нет";
       }
@@ -53,7 +89,11 @@ const OrgStructureTable = ({ onShowEditor }) => {
       key: "actions",
       render: (_, record) => {
         return (
-          <Typography.Link onClick={() => onShowEditor(record.id)}>
+          <Typography.Link
+            onClick={() => {
+              return onShowEditor(record.id);
+            }}
+          >
             Редактировать
           </Typography.Link>
         );
@@ -67,14 +107,16 @@ const OrgStructureTable = ({ onShowEditor }) => {
         <Spinner />
       ) : (
         <Table
-          title={() => (
-            <div className={"flex-between"}>
-              <h4>Орг структура</h4>
-              <Button type={"primary"} onClick={onShowEditor}>
-                Новая орг структура
-              </Button>
-            </div>
-          )}
+          title={() => {
+            return (
+              <div className="flex-between">
+                <h4>Орг структура</h4>
+                <Button type="primary" onClick={onShowEditor}>
+                  Новая орг структура
+                </Button>
+              </div>
+            );
+          }}
           bordered={true}
           dataSource={orgStructures}
           columns={columns}

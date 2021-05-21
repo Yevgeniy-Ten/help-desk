083c61e958826853274c5c41a30768055ae0fe2b
Kuatzhan Boyandinov <kuat.alimov@gmail.com>
2021-05-21 12:43:57 +0600

    #58 слил ветку 56-data в ветку 58-тикет

--- packages/frontend/src/containers/FAQ/FAQ.jsx
+++ packages/frontend/src/containers/FAQ/FAQ.jsx
@@ -12,9 +12,7 @@ const FAQ = () => {
           <div className={styles.card}>
             <Avatar
               size={128}
-              src={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Mo_yec-4l6efC3W2cmFzt7c0E-nKhKYkjA&usqp=CAU"
-              }
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Mo_yec-4l6efC3W2cmFzt7c0E-nKhKYkjA&usqp=CAU"
             />
             <h5>Сайты</h5>
           </div>
@@ -23,9 +21,7 @@ const FAQ = () => {
           <div className={styles.card}>
             <Avatar
               size={128}
-              src={
-                "https://img2.freepng.ru/20180329/iaq/kisspng-accounting-accountant-computer-icons-bookkeeping-f-finance-5abc92d426a2b6.3434380115223077961583.jpg"
-              }
+              src="https://img2.freepng.ru/20180329/iaq/kisspng-accounting-accountant-computer-icons-bookkeeping-f-finance-5abc92d426a2b6.3434380115223077961583.jpg"
             />
             <h5>Бухгалтерия</h5>
           </div>
@@ -34,9 +30,7 @@ const FAQ = () => {
           <div className={styles.card}>
             <Avatar
               size={128}
-              src={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq2LtRn8UowmBomodLPh2jo04ODGP8DDJD4w&usqp=CAU"
-              }
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq2LtRn8UowmBomodLPh2jo04ODGP8DDJD4w&usqp=CAU"
             />
             <h5>Техническая поддержка</h5>
           </div>
@@ -45,9 +39,7 @@ const FAQ = () => {
           <div className={styles.card}>
             <Avatar
               size={128}
-              src={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_K8YJfZwPU1GhJ3xS3FVYnZ2jqMr_CDOwZQ&usqp=CAU"
-              }
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_K8YJfZwPU1GhJ3xS3FVYnZ2jqMr_CDOwZQ&usqp=CAU"
             />
             <h5>Медицина</h5>
           </div>
